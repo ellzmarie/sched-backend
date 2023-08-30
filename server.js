fcc0e65cad75745c94d0d11216b7c12f8c8331a7
Ellie-Marie Matutis-Digal <ellie-mariematutis-digal@Ellie-Maries-MacBook-Air.local>
2023-08-29 18:49:19 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,82 @@
+// DEPENDENCIES  
+
+// GET .env vars
+require("dotenv").config()
+
+// GET PORT AND DB URL 
+const { PORT, DATABASE_URL } = process.env 
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const morgan = require("morgan")
+const cors = require("cors")
+
+// DATABASE CONNECTION
+mongoose.connect(DATABASE_URL)
+// CONNECTION EVENTS
+mongoose.connection
+    .on("open", () => { console.log("You are connected to mongodb") })
+    .on("close", () => { console.log("You are disconnected") })
+    .on("error", (error) => { console.log("error") })
+
+// MODEL
+const ContactsSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: String,
+    timezone: String
+})
+
+const Contact = mongoose.model("Contact", ContactsSchema)
+
+// MIDDLEWARE
+app.use(cors()) // prevents cross origin region sharing errors, allows to server from origin i.e. react frontend
+app.use(morgan("dev")) // logs details of all server hits to terminal
+app.use(express.json())
+app.use(express.urlencoded({ extended: false })) // to use URL encoded 
+ 
+// ROUTES - REMEMBER INDUCES > WE'LL BE USING IDUC
+app.get("/", (req, res) => {
+    res.send("hello world!!!")
+})
+
+// INDEX
+app.get("/contact", async(req, res) => {
+  try {
+    res.status(200).json(await Contact.find({}))
+  } catch (error){
+    res.status(400).json(error)
+  }
+})
+
+// CREATE
+app.post("/contact", async(req, res) => {
+  try {
+    res.status(200).json(await Contact.create(req.body))
+  } catch (error){
+    res.status(400).json(error)
+  } 
+})
+
+// DELETE
+app.delete("/contact/:id", async(req, res) => {
+  try {
+    res.status(200).json(await Contact.findByIdAndDelete(req.params.id))
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+// UPDATE 
+app.put("/contact/:id", async(req, res) => {
+  try {
+    res.status(200).json(await Contact.findByIdAndUpdate(req.params.id, req.body, { new:true }))
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+
+
+// LISTENER 
+app.listen(PORT, () => console.log(`Listening to the smooth sounds of port ${PORT}`))
